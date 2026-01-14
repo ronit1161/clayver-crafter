65cca1c99e1fc0c0baad6bccc57d197ff9896560
Ronit <ronittambe116@gmail.com>
2026-01-14 17:49:58 +0530

    added more products

--- src/app/page.js
+++ src/app/page.js
@@ -57,7 +57,7 @@ export default function Home() {
             className="relative h-[500px] rounded-3xl overflow-hidden shadow-xl"
           >
              <Image
-              src="https://images.unsplash.com/photo-1549488497-657198a44c79?q=80&w=1000&auto=format&fit=crop"
+              src="/images/hero-bg.png"
               alt="Hands working with clay"
               fill
               className="object-cover"
